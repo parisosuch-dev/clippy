686a9772ca36443bdba048af6c9cc3923cc50d0c
paris osuch <paris.osuch@gmail.com>
2024-11-22 21:15:05 -0800

    some auth handling

--- components/nav-bar.tsx
+++ components/nav-bar.tsx
@@ -1,19 +1,42 @@
+"use client";
+
 import Link from "next/link";
 import { Button } from "./ui/button";
+import { signIn, signOut } from "next-auth/react";
+import { useSession } from "next-auth/react";
 
 export default function Navbar() {
+  const { data: session } = useSession();
+
   return (
     <div className="flex flex-row justify-between w-full p-8">
-      <p className="text-clippy-primary font-bold text-2xl">clippy</p>
+      <Link href="/" className="text-clippy-primary font-bold text-2xl">
+        clippy
+      </Link>
       <div className="space-x-8 text-lg">
         <Link href="#">invite bot</Link>
         <Link href="#">guide</Link>
-        <Button
-          variant="outline"
-          className="text-lg border-clippy-accent text-clippy-accent hover:bg-clippy-accent hover:text-white"
-        >
-          login
-        </Button>
+        {session ? (
+          <Button
+            variant="outline"
+            className="text-lg border-clippy-accent text-clippy-accent hover:bg-clippy-accent hover:text-white"
+            onClick={() => {
+              signOut();
+            }}
+          >
+            logout
+          </Button>
+        ) : (
+          <Button
+            variant="outline"
+            className="text-lg border-clippy-accent text-clippy-accent hover:bg-clippy-accent hover:text-white"
+            onClick={() => {
+              signIn("discord");
+            }}
+          >
+            login
+          </Button>
+        )}
       </div>
     </div>
   );
