b0b1ca1d64403f8987803367506376350ced2e76
paris osuch <paris.osuch@gmail.com>
2024-11-23 10:24:59 -0800

    redo landing page

--- app/page.tsx
+++ app/page.tsx
@@ -8,22 +8,22 @@ import { BsDiscord } from "react-icons/bs";
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center w-full">
-      <div className="w-full flex-1 pt-16 flex flex-col items-center justify-center">
-        <div className="w-1/2 text-center">
-          <h1 className="text-clippy-primary font-black text-7xl tracking-wide">
+      <div className="w-1/2 pt-16 flex flex-col items-center justify-center">
+        <Image src={wave} alt="purple wave" className="w-full" />
+        <div className="bg-clippy-primary text-center text-white p-8 rounded-b-xl">
+          <h1 className="font-black text-7xl tracking-wide">
             extra pins for discord channels.
           </h1>
-          <p className="text-lg text-black/75 mt-6">
+          <p className="text-lg text-white mt-6">
             clippy is a discord bot that allows you to add more pins to your
             discord server - called clips.
           </p>
         </div>
       </div>
       <div className="w-1/2 pt-12">
-        <Image src={wave} alt="purple wave" className="w-full" />
-        <div className="h-72 bg-clippy-primary w-full rounded-b-lg flex flex-col items-center justify-center">
-          <h2 className="text-white text-4xl font-bold">how clippy works</h2>
-          <ol className="text-white mt-6 text-lg text-center">
+        <div className="h-72 w-full rounded-b-lg flex flex-col items-center justify-center">
+          <h2 className="text-black text-4xl font-bold">how clippy works</h2>
+          <ol className="text-black mt-6 text-lg text-center">
             <li>1. invite clippy to your discord server.</li>
             <li>
               2. clip a message using discord commands or the context menu.
