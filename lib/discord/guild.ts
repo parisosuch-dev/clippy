95dc66734e2259be1f89ef62b3fe2f5e01b3424b
paris osuch <paris.osuch@gmail.com>
2024-11-24 13:39:27 -0800

    create discord lib and getUserGuilds() function

---
+++ lib/discord/guild.ts
@@ -0,0 +1,28 @@
+import { headers } from "next/headers";
+
+export interface Guild {
+  id: string;
+  name: string;
+  icon: string;
+  banner?: string;
+  owner: boolean;
+  permissions: number;
+  permissions_new: number;
+  features: string[];
+}
+
+export const getUserGuilds = async () => {
+  const response = await fetch(process.env.NEXTAUTH_URL + "/api/user/guilds", {
+    method: "GET",
+    headers: await headers(),
+  });
+
+  if (response.status !== 200) {
+    const text = await response.text();
+    throw new Error(text);
+  }
+
+  const data = await response.json();
+
+  return data as Guild[];
+};
