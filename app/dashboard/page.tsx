49a54b2522d0d98b9b2d715b0e20d3d1127bfe47
paris osuch <paris.osuch@gmail.com>
2024-11-24 14:01:04 -0800

    gut dashboard page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,8 +1,6 @@
 import { getServerSession } from "next-auth";
 import { authOptions } from "../api/auth/[...nextauth]/route";
 import { redirect } from "next/navigation";
-import { getGuildIconURL, getUserGuilds } from "@/lib/discord/guild";
-import Image from "next/image";
 
 export default async function Dashboard() {
   const session = await getServerSession(authOptions);
@@ -10,19 +8,5 @@ export default async function Dashboard() {
   if (!session) {
     redirect("/");
   }
-
-  const guilds = await getUserGuilds();
-
-  return (
-    <div>
-      {guilds.map((guild) => (
-        <Image
-          src={getGuildIconURL(guild)}
-          alt="Discord server icon"
-          width={48}
-          height={48}
-        />
-      ))}
-    </div>
-  );
+  return <div>this is the dashboard page</div>;
 }
