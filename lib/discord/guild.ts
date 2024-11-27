cc58f1eb618f74db01014cf40442798d76cef0f1
paris osuch <paris.osuch@gmail.com>
2024-11-26 18:53:20 -0800

    create getUserGuild() function

--- lib/discord/guild.ts
+++ lib/discord/guild.ts
@@ -27,6 +27,27 @@ export const getUserGuilds = async () => {
   return data as Guild[];
 };
 
+export const getUserGuild = async (id: string) => {
+  const response = await fetch(process.env.NEXTAUTH_URL + "/api/user/guilds", {
+    method: "GET",
+    headers: await headers(),
+  });
+
+  if (response.status !== 200) {
+    const text = await response.text();
+    throw new Error(text);
+  }
+
+  const data = (await response.json()) as Guild[];
+
+  for (const guild of data) {
+    if (id === guild.id) {
+      return guild;
+    }
+  }
+  throw new Error(`Guild with ID ${id} not found.`);
+};
+
 export const getGuildIconURL = (guild: Guild) => {
   return `https://cdn.discordapp.com/icons/${guild.id}/${guild.icon}.png`;
 };
